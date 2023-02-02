b7e6a84ec31eebae38ca8016f0372b65c7fcaa87
jenielseneth <jenielsen@ethz.ch>
2023-02-02 12:32:32 +0100

    par for loop

--- julia_set.cpp
+++ julia_set.cpp
@@ -7,6 +7,7 @@
 #include <omp.h>
 #include <vector>
 
+#define n 1
 // Use high resolution for benchmarking:
 // constexpr int WIDTH = 3840;
 // constexpr int HEIGHT = 2160 / 2;
@@ -30,6 +31,8 @@ void julia_set(void) {
     auto t0 = std::chrono::steady_clock::now();
 
     // TODO: Parallelize.
+    #pragma omp parallel for collapse(2) num_threads(n)
+    {
     for (int i = 0; i < HEIGHT; ++i)
     for (int j = 0; j < WIDTH; ++j) {
         // Compute `w = z_0 = x + i y` for the given pixel (j, i).
@@ -46,6 +49,7 @@ void julia_set(void) {
             ++count;
         }
         image[i][j] = count;
+    }
     }
     auto t1 = std::chrono::steady_clock::now();
     std::chrono::duration<double> diff = t1 - t0;
